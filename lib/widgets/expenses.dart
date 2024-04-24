16a19d95cf0aa807c5c9e679c2fb59964954fd4a
ishita1226 <ishitashukla120603@gmail.com>
2024-04-25 00:28:34 +0530

    completed

--- lib/widgets/expenses.dart
+++ lib/widgets/expenses.dart
@@ -1,42 +1,102 @@
-import 'package:expense_tracker/widgets/expenses_list/expenses_list.dart';
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:expense_tracker/models/expense.dart';
 
-class Expenses extends StatefulWidget{
+import 'package:expense_tracker/widgets/new_expense.dart';
+import 'package:expense_tracker/widgets/expenses_list/expenses_list.dart';
+import 'package:expense_tracker/models/expense.dart';
+import 'package:expense_tracker/widgets/chart/chart.dart';
+
+class Expenses extends StatefulWidget {
   const Expenses({super.key});
+
   @override
-  State<StatefulWidget> createState() {
+  State<Expenses> createState() {
     return _ExpensesState();
   }
 }
 
-class _ExpensesState extends State<Expenses>{
+class _ExpensesState extends State<Expenses> {
   final List<Expense> _registeredExpenses = [
     Expense(
-        title: 'books',
-        amount: 500,
-        date: DateTime.now(),
-        category: Category.work,
+      title: 'Flutter Course',
+      amount: 999,
+      date: DateTime.now(),
+      category: Category.work,
     ),
     Expense(
-      title: 'cinema',
-      amount: 400,
+      title: 'Cinema',
+      amount: 369,
       date: DateTime.now(),
       category: Category.leisure,
     ),
   ];
+
+  void _openAddExpenseOverlay() {
+    showModalBottomSheet(
+      isScrollControlled: true,
+      context: context,
+      builder: (ctx) => NewExpense(onAddExpense: _addExpense),
+    );
+  }
+
+  void _addExpense(Expense expense) {
+    setState(() {
+      _registeredExpenses.add(expense);
+    });
+  }
+
+  void _removeExpense(Expense expense) {
+    final expenseIndex = _registeredExpenses.indexOf(expense);
+    setState(() {
+      _registeredExpenses.remove(expense);
+    });
+    ScaffoldMessenger.of(context).clearSnackBars();
+    ScaffoldMessenger.of(context).showSnackBar(
+      SnackBar(
+        duration: const Duration(seconds: 3),
+        content: const Text('Expense deleted.'),
+        action: SnackBarAction(
+          label: 'Undo',
+          onPressed: () {
+            setState(() {
+              _registeredExpenses.insert(expenseIndex, expense);
+            });
+          },
+        ),
+      ),
+    );
+  }
+
   @override
-  Widget build(BuildContext context){
-    return Scaffold(body:
-    Column(
-      children: [
-        const Text('Chart value'),
-        Expanded(
-            child: ExpensesList(expenses: _registeredExpenses,)
-        )
-      ],
-    ),
+  Widget build(BuildContext context) {
+    Widget mainContent = const Center(
+      child: Text('No expenses found. Start adding some!'),
+    );
+
+    if (_registeredExpenses.isNotEmpty) {
+      mainContent = ExpensesList(
+        expenses: _registeredExpenses,
+        onRemoveExpense: _removeExpense,
+      );
+    }
+
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Flutter ExpenseTracker'),
+        actions: [
+          IconButton(
+            onPressed: _openAddExpenseOverlay,
+            icon: const Icon(Icons.add),
+          ),
+        ],
+      ),
+      body: Column(
+        children: [
+          Chart(expenses: _registeredExpenses),
+          Expanded(
+            child: mainContent,
+          ),
+        ],
+      ),
     );
   }
 }
--- lib/widgets/expenses_list/expense_item.dart
+++ lib/widgets/expenses_list/expense_item.dart
@@ -1,8 +1,10 @@
 import 'package:flutter/material.dart';
+
 import 'package:expense_tracker/models/expense.dart';
 
-class ExpenseItem extends StatelessWidget{
-  const ExpenseItem(this.expense,{super.key});
+class ExpenseItem extends StatelessWidget {
+  const ExpenseItem(this.expense, {super.key});
+
   final Expense expense;
 
   @override
@@ -11,12 +13,34 @@ class ExpenseItem extends StatelessWidget{
       child: Padding(
         padding: const EdgeInsets.symmetric(
           horizontal: 20,
-          vertical: 16
+          vertical: 16,
         ),
-        child: Text(expense.title),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text(
+              expense.title,
+              style: Theme.of(context).textTheme.titleLarge,
+            ),
+            const SizedBox(height: 4),
+            Row(
+              children: [
+                Text(
+                  'Rs. ${expense.amount.toStringAsFixed(2)}',
+                ),
+                const Spacer(),
+                Row(
+                  children: [
+                    Icon(categoryIcons[expense.category]),
+                    const SizedBox(width: 8),
+                    Text(expense.formattedDate),
+                  ],
+                ),
+              ],
+            ),
+          ],
+        ),
       ),
     );
   }
-
-
 }
